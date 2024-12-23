9a7522f73f4f0d9914c1ff82609ffee5618466e0
Legend <63176640+asus3020@users.noreply.github.com>
2024-12-23 14:54:55 +0300

    Alpha Testing 1.2

--- Scripts/panel.gd
+++ Scripts/panel.gd
@@ -11,6 +11,8 @@ func _on_decrease_button_pressed() -> void:
 	$NumberScoreLabel.text=str(number)
 	pass # Replace with function body.
 
+
+
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
 	pass # Replace with function body.
---
+++ Scripts/settings_panel.gd
@@ -0,0 +1,6 @@
+extends Panel
+
+
+func _on_back_in_settings_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+	pass # Replace with function body.
