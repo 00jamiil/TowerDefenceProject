a5f8587fc0a32dc79de660e577679f53d173882f
Legend <63176640+asus3020@users.noreply.github.com>
2024-12-23 14:20:44 +0300

    Alpha Testing 1.1

--- Scripts/menu_panel.gd
+++ Scripts/menu_panel.gd
@@ -36,14 +36,46 @@
 
 #------------------------This is my new Code for the buttons ---------------------------------
 extends Panel
-func _on_play_button_mouse_entered() -> void:
-	$PlayButton.scale = Vector2(1.25, 1.25)
-	$PlayButton.position = Vector2(132-12, 64-8.625)
-	
-	pass
 
+func _on_play_button_mouse_entered() -> void:				#Play button Hover
+	$PlayButton.scale = Vector2(1.25, 1.25)					#Play button Hover
+	$PlayButton.position = Vector2(132-12, 64-8.625)		#Play button Hover
+	pass													#Play button Hover
 	
-func _on_play_button_mouse_exited() -> void:
-	$PlayButton.scale = Vector2(1, 1)
-	$PlayButton.position = Vector2(132, 64)
-	pass
+func _on_play_button_mouse_exited() -> void:				#Play button Exit Hover
+	$PlayButton.scale = Vector2(1, 1)						#Play button Exit Hover
+	$PlayButton.position = Vector2(132, 64)					#Play button Exit Hover
+	pass													#Play button Exit Hover
+
+func _on_settings_button_mouse_entered() -> void:			#Settings button Hover
+	$SettingsButton.scale = Vector2(1.25, 1.25)				#Settings button Hover
+	$SettingsButton.position = Vector2(81.5-20.375, 192-6)	#Settings button Hover
+	pass # Replace with function body.						#Settings button Hover
+
+func _on_settings_button_mouse_exited() -> void:			#Settings button Exit Hover
+	$SettingsButton.scale = Vector2(1, 1)					#Settings button Exit Hover
+	$SettingsButton.position = Vector2(81.5, 192)			#Settings button Exit Hover
+	pass # Replace with function body.						#Settings button Exit Hover
+
+func _on_exit_button_mouse_entered() -> void:				#Exit button Hover
+	$ExitButton.scale = Vector2(1.25, 1.25)					#Exit button Hover
+	$ExitButton.position = Vector2(132-11, 320-10)			#Exit button Hover
+	pass # Replace with function body.						#Exit button Hover
+
+func _on_exit_button_mouse_exited() -> void:				#Exit button Exit Hover
+	$ExitButton.scale = Vector2(1, 1)						#Exit button Exit Hover
+	$ExitButton.position = Vector2(132, 320)				#Exit button Exit Hover
+	pass # Replace with function body.						#Exit button Exit Hover
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Testing clicks.tscn")
+	pass # Replace with function body.
+
+
+func _on_settings_button_pressed() -> void:
+	
+	pass # Replace with function body.
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()										#Quit the game when Exit is pressed (Might add a conform screen)
+	pass # Replace with function body.
--- Scripts/panel.gd
+++ Scripts/panel.gd
@@ -10,3 +10,7 @@ func _on_decrease_button_pressed() -> void:
 	number=int(number)-1
 	$NumberScoreLabel.text=str(number)
 	pass # Replace with function body.
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+	pass # Replace with function body.
